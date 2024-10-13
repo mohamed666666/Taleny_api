3934ca61b89aef0c7b994f37f25dd537effbeca9
v <v@kali>
2024-10-13 15:57:45 +0300

    delete unimportant file with ip name

--- userservice/models/follow.py
+++ userservice/models/follow.py
@@ -6,3 +6,6 @@ class Follow(models.Model):
     follow_from=models.ForeignKey(UserBase,on_delete=models.CASCADE,related_name='follow_from')
     follow_to=models.ForeignKey(UserBase,on_delete=models.CASCADE,related_name='Follow_to')
     status=models.BooleanField(default=False)
+    
+    class Meta:
+        unique_together =('follow_from','follow_to')
